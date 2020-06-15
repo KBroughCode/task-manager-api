2df2ea3c4062d593449e016536e36cf76df60c4a
Kris Brough <a0109728@0109728s-mbp.home>
2020-06-15 16:20:53 +0100

    env variable updated for database. should fix issues in prod

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,46 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
-
-// const me = new User({
-//   name: "Mitch",
-//   email: "mitch@hotmail.com",
-//   password: "Misty321"
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log(error, "an error");
-//   });
-
-// const Task = new mongoose.model("Task", {
-//   description: {
-//     type: String,
-//     required: true,
-//     trim: true
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false
-//   }
-// });
-
-// const shopping = new Task({
-//   description: "                             Buy food               "
-// });
-
-// shopping
-//   .save()
-//   .then(() => {
-//     console.log(shopping);
-//   })
-//   .catch(error => {
-//     console.log(error, "shopping error");
-//   });
--- src/index.js
+++ src/index.js
@@ -6,28 +6,6 @@ const taskRouter = require("./routers/task");
 
 const port = process.env.PORT;
 
-// Blocking requests
-// app.use((req, res, next) => {
-//   if (req.method === "GET") {
-//     res.send("get requests disabled");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   if (
-//     req.method === "GET" ||
-//     req.method === "POST" ||
-//     req.method === "PATCH" ||
-//     req.method === "DELETE"
-//   ) {
-//     res.status(503).send("Cannot process request as maintainence taking place");
-//   } else {
-//     next();
-//   }
-// });
-
 const multer = require("multer");
 const upload = multer({
   dest: "images"
@@ -43,20 +21,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
-
-//Playing with populate
-
-// const Task = require("./models/task");
-// const User = require("./models/user");
-
-// const main = async () => {
-//   // const task = await Task.findById("5ed61ca74175d7a087a97dc3");
-//   // await task.populate('owner').execPopulate()
-//   // console.log(task.owner);
-
-//   const user = await User.findById("5ed619864175d7a087a97dc1");
-//   await user.populate("tasks").execPopulate();
-//   console.log(user.tasks);
-// };
-
-// main();
